96ae1dc7d645ebd2d718c37d21194a20720fc417
Takafumi Arakaki <aka.tkf@gmail.com>
2020-02-21 18:03:45 -0800

    Add bench_filter_map.jl

---
+++ benchmark/bench_filter_map.jl
@@ -0,0 +1,34 @@
+module BenchFilterMap
+
+using BenchmarkTools
+using Restacker: restack
+
+@noinline function filter_map_norestack!(ys, xs)
+    @inbounds for i in eachindex(ys, xs)
+        x = xs[i]
+        if -0.5 < x < 0.5
+            ys[i] = 2x
+        end
+    end
+end
+
+@noinline function filter_map_restack!(ys0, xs0)
+    ys = restack(ys0)
+    xs = restack(xs0)
+    @inbounds for i in eachindex(ys, xs)
+        x = xs[i]
+        if -0.5 < x < 0.5
+            ys[i] = 2x
+        end
+    end
+end
+
+suite = BenchmarkGroup()
+
+xs = view(randn(10_000), :)
+ys = view(randn(10_000), :)
+suite["norestack"] = @benchmarkable filter_map_norestack!($ys, $xs)
+suite["restack"] = @benchmarkable filter_map_restack!($ys, $xs)
+
+end  # module
+BenchFilterMap.suite
